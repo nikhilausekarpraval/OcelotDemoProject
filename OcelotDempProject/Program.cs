ddeccc4ca6e33d82b7794fc7eabc548b9ac73282
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-09-30 14:18:48 +0530

    updated program.cs

--- OcelotDempProject/Program.cs
+++ OcelotDempProject/Program.cs
@@ -6,12 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+
 builder.Services.AddOcelot(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
+
+app.UseRouting();
+
+app.UseAuthorization();
 
 await app.UseOcelot();
 
